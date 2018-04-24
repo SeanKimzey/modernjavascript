b14b0e5706aff1b6d6903f050444934cc819dc8e
Sean Kimzey <sean.kimzey@gmail.com>
2018-04-24 18:41:26 -0500

    stated modern js app with webpack installed and babel installed

---
+++ index.js
@@ -0,0 +1,15 @@
+var moment = require('moment');
+var math = require('mathjs');
+
+
+console.log('hello');
+
+console.log(moment().format('llll'));
+
+math.sqrt(-4);
+console.log(math.sqrt(-4));
+
+var name =  "Sean";
+var last =  "Kimzey";
+
+console.log(`${name} ${last}`);
